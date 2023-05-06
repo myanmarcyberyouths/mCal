850948155e9d2f74ebb4bfc0434c06ba910ee23a
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-07 03:20:11 +0630

    chore(next): update to nextjs latest version with app router (#12)

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+    // console.log(new URL(import.meta.url.pa))
+    // console.log(await fs.readFile(path.join(new URL(import.meta.url).pathname, '../', 'index.html'), 'utf8'))
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
