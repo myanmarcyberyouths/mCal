de41f1f1d46cdc4bfc852155a25f308dc422ed12
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-09 22:14:27 +0630

    feat: add new ui of calendar

---
+++ src/utils/engToMyanmarNumber.ts
@@ -0,0 +1,10 @@
+export function engToMyanmarNumber(num: number | string) {
+    const myanmarNumbers = ['၀', '၁', '၂', '၃', '၄', '၅', '၆', '၇', '၈', '၉'];
+    const numStr = String(num);
+    let result = '';
+    for (let i = 0; i < numStr.length; i++) {
+        const digit = parseInt(numStr.charAt(i));
+        result += myanmarNumbers[digit];
+    }
+    return result;
+}
