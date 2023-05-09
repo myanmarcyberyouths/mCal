31bfa1b07a924689613eec70cc19865a86df9ee1
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-09 17:22:01 +0630

    fix(chore): fix next config export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 
 const nextConfig = {
     reactStrictMode: true,
+    output: "export",
     // Note: This feature is required to use NextJS Image in SSG mode.
     // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
     images: {
