25dd040b35842582c08f725401f632324b406a74
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-09 17:16:29 +0630

    feat: add new ui of myanmar calendar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
