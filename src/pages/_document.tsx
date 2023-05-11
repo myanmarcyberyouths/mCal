8c20e17ea676f7e86b25a5b9fa5f38fac2832ae3
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-12 00:10:05 +0630

    fix: add meta tag for responsive

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,10 @@ import {Html, Head, Main, NextScript} from 'next/document';
 export default function Document() {
     return (
         <Html>
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1, maximum-scale=1"
+            ></meta>
             <Head/>
             <body>
             <Main/>
