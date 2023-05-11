e3cb5fe34a600cbb58c616af7131c9dd5153d592
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-11 23:48:18 +0630

    feat: add full moon icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import React, { useEffect } from "react";
+import React from 'react';
 import MyanmarCalendar from "../components/MyanmarCalendar";
-import { useKeyDown } from "../hooks/useKeyDown";
 
 const Index = () => {
-  return (
-    <div>
-      <MyanmarCalendar />
-    </div>
-  );
+    return (
+        <MyanmarCalendar/>
+    );
 };
 
 export default Index;
