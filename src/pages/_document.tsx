6965e5f7a14310d0ee7f0d7c33c0ab7c2ee2dec6
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-12 00:13:28 +0630

    fix: meta wrong closing tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,7 @@ import {Html, Head, Main, NextScript} from 'next/document';
 export default function Document() {
     return (
         <Html>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, maximum-scale=1"
-            ></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <Head/>
             <body>
             <Main/>
