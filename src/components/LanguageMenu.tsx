c071212cf393423fc765a507952a2989ac480913
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-19 00:25:43 +0630

    feat: add blur effect for mac and windows

--- src/components/LanguageMenu.tsx
+++ src/components/LanguageMenu.tsx
@@ -21,7 +21,7 @@ const LanguageMenu = ({
                 type="button"
                 className="capitalize flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
             >
-                <LanguageIcon/>
+                <LanguageIcon className="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true"/>
                 {selectedLanguage}
                 <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true"/>
             </Menu.Button>
