7849a1d6a69c3568a9572bc5c80f2aa53b49aea3
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-19 00:37:28 +0630

    feat: add vercel insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import type {AppProps} from 'next/app';
+import {Analytics} from '@vercel/analytics/react';
 import "../assets/styles/globals.css"
 
-export default function MyApp({Component, pageProps}) {
-    return <Component {...pageProps} />;
+export default function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics/>
+        </>
+    );
 }
