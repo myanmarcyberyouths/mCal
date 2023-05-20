be36358ba93363bfb8e85cfdf4ddcc688e6ce61c
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-05-21 00:17:24 +0630

    feat(seo): add meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,22 @@ export default function Document() {
     return (
         <Html>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>Myanmar Calendar -Myanmar Calendar is a feature-rich calendar app that seamlessly combines the
+                functionality of Gregorian and Myanmar calendars, catering to users who follow both systems. It supports
+                multiple languages, including Myanmar, Mon, English, Tai, and Karen, ensuring a user-friendly experience
+                for individuals from diverse linguistic backgrounds.</title>
+            <meta name="title" content="Myanmar Calendar "/>
+            <meta name="description"
+                  content="Myanmar Calendar is a feature-rich calendar app that seamlessly combines the functionality of Gregorian and Myanmar calendars, catering to users who follow both systems. It supports multiple languages, including Myanmar, Mon, English, Tai, and Karen, ensuring a user-friendly experience for individuals from diverse linguistic backgrounds."/>
+            <meta name="keywords" content="myanmar-calendar, caleadar, burma calendar, burmese calendar"/>
+            <meta name="robots" content="index, follow"/>
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
+            <meta name="language" content="English"/>
+            <meta name="revisit-after" content="1 days"/>
+            <meta name="author" content="Aung Myat Moe"/>
+            <meta content="width=device-width, initial-scale=1" name="viewport"/>
+            <meta name="description" content="Aung Myat Moe - Software Engineer, Founder and Open Source Enthusiast"/>
+            <meta name="robots" content="all"/>
             <Head/>
             <body>
             <Main/>
