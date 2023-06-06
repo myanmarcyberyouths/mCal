de4bb97d5f95c71ba782c675404694ea182e60f0
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-06-07 01:22:35 +0630

    fix: unknow type in LanguageMenu

--- apps/myanmar_calendar/src/components/LanguageMenu.tsx
+++ apps/myanmar_calendar/src/components/LanguageMenu.tsx
@@ -22,7 +22,7 @@ const LanguageMenu = ({
                 className="capitalize flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
             >
                 <LanguageIcon className="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true"/>
-                {selectedLanguage}
+                {selectedLanguage as any}
                 <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true"/>
             </Menu.Button>
 
