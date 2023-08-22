13e45bbe7d11051ab155d4bdf4c06abe3a39cdde
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-08-22 11:27:02 +0630

    refactor: remove dead code

--- apps/myanmar_calendar/src/components/MyanmarCalendar.tsx
+++ apps/myanmar_calendar/src/components/MyanmarCalendar.tsx
@@ -1,6 +1,5 @@
 import React, {Fragment, useEffect, useState} from "react";
-import {ChevronLeftIcon, ChevronRightIcon, EllipsisHorizontalIcon,} from "@heroicons/react/20/solid";
-import {Menu, Transition} from "@headlessui/react";
+import {ChevronLeftIcon, ChevronRightIcon,} from "@heroicons/react/20/solid";
 import {
     add,
     eachDayOfInterval,
@@ -150,92 +149,6 @@ export default function MyanmarCalendar() {
                                 <ChevronRightIcon className="h-5 w-5" aria-hidden="true"/>
                             </button>
                         </div>
-                        {/*<div className="hidden md:ml-4 md:flex md:items-center">*/}
-                        {/*    <Menu as="div" className="relative">*/}
-                        {/*        <Menu.Button*/}
-                        {/*            type="button"*/}
-                        {/*            className="flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"*/}
-                        {/*        >*/}
-                        {/*            Month view*/}
-                        {/*            <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true"/>*/}
-                        {/*        </Menu.Button>*/}
-
-                        {/*        <Transition*/}
-                        {/*            as={Fragment}*/}
-                        {/*            enter="transition ease-out duration-100"*/}
-                        {/*            enterFrom="transform opacity-0 scale-95"*/}
-                        {/*            enterTo="transform opacity-100 scale-100"*/}
-                        {/*            leave="transition ease-in duration-75"*/}
-                        {/*            leaveFrom="transform opacity-100 scale-100"*/}
-                        {/*            leaveTo="transform opacity-0 scale-95"*/}
-                        {/*        >*/}
-                        {/*            <Menu.Items*/}
-                        {/*                className="absolute right-0 z-10 mt-3 w-36 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">*/}
-                        {/*                <div className="py-1">*/}
-                        {/*                    <Menu.Item>*/}
-                        {/*                        {({active}) => (*/}
-                        {/*                            <a*/}
-                        {/*                                href="#"*/}
-                        {/*                                className={classNames(*/}
-                        {/*                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',*/}
-                        {/*                                    'block px-4 py-2 text-sm'*/}
-                        {/*                                )}*/}
-                        {/*                            >*/}
-                        {/*                                Day view*/}
-                        {/*                            </a>*/}
-                        {/*                        )}*/}
-                        {/*                    </Menu.Item>*/}
-                        {/*                    <Menu.Item>*/}
-                        {/*                        {({active}) => (*/}
-                        {/*                            <a*/}
-                        {/*                                href="#"*/}
-                        {/*                                className={classNames(*/}
-                        {/*                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',*/}
-                        {/*                                    'block px-4 py-2 text-sm'*/}
-                        {/*                                )}*/}
-                        {/*                            >*/}
-                        {/*                                Week view*/}
-                        {/*                            </a>*/}
-                        {/*                        )}*/}
-                        {/*                    </Menu.Item>*/}
-                        {/*                    <Menu.Item>*/}
-                        {/*                        {({active}) => (*/}
-                        {/*                            <a*/}
-                        {/*                                href="#"*/}
-                        {/*                                className={classNames(*/}
-                        {/*                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',*/}
-                        {/*                                    'block px-4 py-2 text-sm'*/}
-                        {/*                                )}*/}
-                        {/*                            >*/}
-                        {/*                                Month view*/}
-                        {/*                            </a>*/}
-                        {/*                        )}*/}
-                        {/*                    </Menu.Item>*/}
-                        {/*                    <Menu.Item>*/}
-                        {/*                        {({active}) => (*/}
-                        {/*                            <a*/}
-                        {/*                                href="#"*/}
-                        {/*                                className={classNames(*/}
-                        {/*                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',*/}
-                        {/*                                    'block px-4 py-2 text-sm'*/}
-                        {/*                                )}*/}
-                        {/*                            >*/}
-                        {/*                                Year view*/}
-                        {/*                            </a>*/}
-                        {/*                        )}*/}
-                        {/*                    </Menu.Item>*/}
-                        {/*                </div>*/}
-                        {/*            </Menu.Items>*/}
-                        {/*        </Transition>*/}
-                        {/*    </Menu>*/}
-                        {/*    <div className="ml-6 h-6 w-px bg-gray-300"/>*/}
-                        {/*    <button*/}
-                        {/*        type="button"*/}
-                        {/*        className="ml-6 rounded-md  bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"*/}
-                        {/*    >*/}
-                        {/*        Add event*/}
-                        {/*    </button>*/}
-                        {/*</div>*/}
 
                         <div className="hidden md:ml-4 md:flex md:items-center">
                             <LanguageMenu
@@ -244,111 +157,16 @@ export default function MyanmarCalendar() {
                             />
                         </div>
 
-                        <Menu as="div" className="relative ml-6 md:hidden">
-                            <Menu.Button
-                                className="-mx-2 flex items-center rounded-full border border-transparent p-2 text-gray-400 hover:text-gray-500">
-                                <span className="sr-only">Open menu</span>
-                                <EllipsisHorizontalIcon className="h-5 w-5" aria-hidden="true"/>
-                            </Menu.Button>
-
-                            <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-100"
-                                enterFrom="transform opacity-0 scale-95"
-                                enterTo="transform opacity-100 scale-100"
-                                leave="transition ease-in duration-75"
-                                leaveFrom="transform opacity-100 scale-100"
-                                leaveTo="transform opacity-0 scale-95"
+                        <div className="py-1 sm:hidden">
+                            <button
+                                className={classNames(
+                                    'block px-4 py-2 text-sm text-indigo-500'
+                                )}
+                                onClick={() => setCurrentMonth(format(today, "MMM-yyyy"))}
                             >
-                                <Menu.Items
-                                    className="absolute right-0 z-10 mt-3 w-36 origin-top-right divide-y divide-gray-100 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    {/*<div className="py-1">*/}
-                                    {/*    <Menu.Item>*/}
-                                    {/*        {({active}) => (*/}
-                                    {/*            <a*/}
-                                    {/*                href="#"*/}
-                                    {/*                className={classNames(*/}
-                                    {/*                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',*/}
-                                    {/*                    'block px-4 py-2 text-sm'*/}
-                                    {/*                )}*/}
-                                    {/*            >*/}
-                                    {/*                Create event*/}
-                                    {/*            </a>*/}
-                                    {/*        )}*/}
-                                    {/*    </Menu.Item>*/}
-                                    {/*</div>*/}
-                                    <div className="py-1">
-                                        <Menu.Item>
-                                            {({active}) => (
-                                                <button
-                                                    className={classNames(
-                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}
-                                                    onClick={() => setCurrentMonth(format(today, "MMM-yyyy"))}
-                                                >
-                                                    Go to today
-                                                </button>
-                                            )}
-                                        </Menu.Item>
-                                    </div>
-                                    {/*<div className="py-1">*/}
-                                    {/*    <Menu.Item>*/}
-                                    {/*        {({active}) => (*/}
-                                    {/*            <a*/}
-                                    {/*                href="#"*/}
-                                    {/*                className={classNames(*/}
-                                    {/*                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',*/}
-                                    {/*                    'block px-4 py-2 text-sm'*/}
-                                    {/*                )}*/}
-                                    {/*            >*/}
-                                    {/*                Day view*/}
-                                    {/*            </a>*/}
-                                    {/*        )}*/}
-                                    {/*    </Menu.Item>*/}
-                                    {/*    <Menu.Item>*/}
-                                    {/*        {({active}) => (*/}
-                                    {/*            <a*/}
-                                    {/*                href="#"*/}
-                                    {/*                className={classNames(*/}
-                                    {/*                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',*/}
-                                    {/*                    'block px-4 py-2 text-sm'*/}
-                                    {/*                )}*/}
-                                    {/*            >*/}
-                                    {/*                Week view*/}
-                                    {/*            </a>*/}
-                                    {/*        )}*/}
-                                    {/*    </Menu.Item>*/}
-                                    {/*    <Menu.Item>*/}
-                                    {/*        {({active}) => (*/}
-                                    {/*            <a*/}
-                                    {/*                href="#"*/}
-                                    {/*                className={classNames(*/}
-                                    {/*                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',*/}
-                                    {/*                    'block px-4 py-2 text-sm'*/}
-                                    {/*                )}*/}
-                                    {/*            >*/}
-                                    {/*                Month view*/}
-                                    {/*            </a>*/}
-                                    {/*        )}*/}
-                                    {/*    </Menu.Item>*/}
-                                    {/*    <Menu.Item>*/}
-                                    {/*        {({active}) => (*/}
-                                    {/*            <a*/}
-                                    {/*                href="#"*/}
-                                    {/*                className={classNames(*/}
-                                    {/*                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',*/}
-                                    {/*                    'block px-4 py-2 text-sm'*/}
-                                    {/*                )}*/}
-                                    {/*            >*/}
-                                    {/*                Year view*/}
-                                    {/*            </a>*/}
-                                    {/*        )}*/}
-                                    {/*    </Menu.Item>*/}
-                                    {/*</div>*/}
-                                </Menu.Items>
-                            </Transition>
-                        </Menu>
+                                Today
+                            </button>
+                        </div>
                     </div>
                 </header>
 
@@ -447,7 +265,7 @@ export default function MyanmarCalendar() {
                                         isSameMonth(day, firstDayCurrentMonth) ? 'bg-white' : 'bg-gray-50',
                                         isToday(day) && 'font-semibold',
                                         isToday(day) && 'text-indigo-600',
-                                        'flex h-14 flex-col px-3 py-2 hover:bg-gray-100 focus:z-10'
+                                        'flex h-24 flex-col px-3 py-2 hover:bg-gray-100 focus:z-10'
                                     )}
                                 >
                                     <time
@@ -457,6 +275,7 @@ export default function MyanmarCalendar() {
                                         )}
                                     >
                                         {format(getLocalTime(day), "d")}
+
                                     </time>
                                 </button>
                             ))}
