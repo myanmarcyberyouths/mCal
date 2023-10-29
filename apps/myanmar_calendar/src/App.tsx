52fa550af2ba9daf70dccf93a72596f9c5b5be4f
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-10-29 23:19:25 +0630

    refactor: pages dir and naming convention

--- apps/myanmar_calendar/src/App.tsx
+++ apps/myanmar_calendar/src/App.tsx
@@ -3,14 +3,19 @@ import MyanmarCalendar2 from "@/components/MyanmarCalendar2";
 import {Provider} from "react-redux";
 import {store} from "@/store";
 import AppSetupProvider from "@/components/providers/AppSetupProvider";
+import {Analytics} from "@vercel/analytics/react";
+
 
 const Home = () => {
     return (
-        <Provider store={store}>
-            <AppSetupProvider>
-                <MyanmarCalendar2/>
-            </AppSetupProvider>
-        </Provider>
+        <>
+            <Analytics/>
+            <Provider store={store}>
+                <AppSetupProvider>
+                    <MyanmarCalendar2/>
+                </AppSetupProvider>
+            </Provider>
+        </>
     );
 };
 
