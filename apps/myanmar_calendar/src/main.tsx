6d02ead9b0e7fe0d891a1d19cf517aca185b1ad1
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-10-29 23:03:11 +0630

    reactor: drop next js

--- apps/myanmar_calendar/src/main.tsx
+++ apps/myanmar_calendar/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './pages/App'
+import './assets/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
--- apps/myanmar_calendar/src/pages/App.tsx
+++ apps/myanmar_calendar/src/pages/App.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import MyanmarCalendar from "../components/MyanmarCalendar";
 import MyanmarCalendar2 from "@/components/MyanmarCalendar2";
-import { Provider } from "react-redux";
-import { store } from "@/store";
+import {Provider} from "react-redux";
+import {store} from "@/store";
 import AppSetupProvider from "@/components/providers/AppSetupProvider";
 
 const Home = () => {
-  return (
-    <Provider store={store}>
-      <AppSetupProvider>
-        {/* <MyanmarCalendar /> */}
-        <MyanmarCalendar2 />
-      </AppSetupProvider>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <AppSetupProvider>
+                <MyanmarCalendar2/>
+            </AppSetupProvider>
+        </Provider>
+    );
 };
 
 export default Home;
