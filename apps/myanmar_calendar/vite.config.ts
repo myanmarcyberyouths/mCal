7d51030ca85c9d412d042c548a863bd13bf73245
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-11-06 11:10:28 +0630

    chore: add pwa

--- apps/myanmar_calendar/vite.config.ts
+++ apps/myanmar_calendar/vite.config.ts
@@ -1,28 +1,74 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "url";
+import {fileURLToPath, URL} from "url";
+import {VitePWA, VitePWAOptions} from "vite-plugin-pwa";
+import {resolve} from "path";
+
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+    registerType: "prompt",
+    includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+    manifest: {
+        name: "mCal",
+        short_name: "mCal",
+        description: "Myanmar calendar application",
+        icons: [
+            {
+                src: "/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+            },
+            {
+                src: "/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+            },
+            {
+                src: "/apple-touch-icon.png",
+                sizes: "180x180",
+                type: "image/png",
+                purpose: "apple touch icon",
+            },
+            {
+                src: "/maskable_icon.png",
+                sizes: "225x225",
+                type: "image/png",
+                purpose: "any maskable",
+            },
+        ],
+        theme_color: "#171717",
+        background_color: "#e8ebf2",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+    },
+};
 
 export default defineConfig({
-  // prevent vite from obscuring rust errors
-  clearScreen: false,
-  // Tauri expects a fixed port, fail if that port is not available
-  server: {
-    strictPort: true,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    // prevent vite from obscuring rust errors
+    clearScreen: false,
+    // Tauri expects a fixed port, fail if that port is not available
+    server: {
+        strictPort: true,
     },
-  },
-  // to access the Tauri environment variables set by the CLI with information about the current target
-  envPrefix: ["VITE_", "TAURI_PLATFORM", "TAURI_ARCH", "TAURI_FAMILY", "TAURI_PLATFORM_VERSION", "TAURI_PLATFORM_TYPE", "TAURI_DEBUG"],
-  build: {
-    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
-  },
+    plugins: [
+        react(),
+        VitePWA(manifestForPlugin)
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    // to access the Tauri environment variables set by the CLI with information about the current target
+    envPrefix: ["VITE_", "TAURI_PLATFORM", "TAURI_ARCH", "TAURI_FAMILY", "TAURI_PLATFORM_VERSION", "TAURI_PLATFORM_TYPE", "TAURI_DEBUG"],
+    build: {
+        // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+        target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+        // don't minify for debug builds
+        minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+        // produce sourcemaps for debug builds
+        sourcemap: !!process.env.TAURI_DEBUG,
+    },
 });
