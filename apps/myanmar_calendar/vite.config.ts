f9e333684b93f8a083c8dffcc8c818a3bb7acdc8
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-11-08 11:09:24 +0630

    feat: add reload pwa (#77)

--- apps/myanmar_calendar/vite.config.ts
+++ apps/myanmar_calendar/vite.config.ts
@@ -1,70 +1,113 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "url";
-import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
-import { resolve } from "path";
+import {fileURLToPath, URL} from "url";
+import {VitePWA, VitePWAOptions} from "vite-plugin-pwa"
+import process from 'node:process'
+import replace from '@rollup/plugin-replace'
+
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
-  registerType: "prompt",
-  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-  manifest: {
-    name: "mCal",
-    short_name: "mCal",
-    description: "Myanmar's first digital calendar",
-    icons: [
-      {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        src: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-        purpose: "apple touch icon",
-      },
-      {
-        src: "/maskable_icon.png",
-        sizes: "225x225",
-        type: "image/png",
-        purpose: "any maskable",
-      },
-    ],
-    theme_color: "#171717",
-    background_color: "#e8ebf2",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    orientation: "portrait",
-  },
+    mode: "development",
+    registerType: "prompt",
+    devOptions: {
+        enabled: process.env.SW_DEV === 'true',
+        /* when using generateSW the PWA plugin will switch to classic */
+        type: 'module',
+        navigateFallback: 'index.html',
+    },
+
+    workbox: {
+        sourcemap: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+    },
+    includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+    manifest: {
+        name: "mCal",
+        short_name: "mCal",
+        description: "Myanmar's first digital calendar",
+        icons: [
+            {
+                src: "/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+            },
+            {
+                src: "/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+            },
+            {
+                src: "/apple-touch-icon.png",
+                sizes: "180x180",
+                type: "image/png",
+                purpose: "apple touch icon",
+            },
+            {
+                src: "/maskable_icon.png",
+                sizes: "225x225",
+                type: "image/png",
+                purpose: "any maskable",
+            },
+        ],
+        theme_color: "#171717",
+        background_color: "#e8ebf2",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+    },
 };
 
+const replaceOptions = {__DATE__: new Date().toISOString()}
+const claims = process.env.CLAIMS === 'true'
+const reload = process.env.RELOAD_SW === 'true'
+const selfDestroying = process.env.SW_DESTROY === 'true'
+
+if (process.env.SW === 'true') {
+    pwaOptions.srcDir = 'src'
+    pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
+    pwaOptions.strategies = 'injectManifest'
+    ;(pwaOptions.manifest as Partial<ManifestOptions>).name = 'PWA Inject Manifest'
+    ;(pwaOptions.manifest as Partial<ManifestOptions>).short_name = 'PWA Inject'
+}
+
+if (claims)
+    pwaOptions.registerType = 'autoUpdate'
+
+if (reload) {
+    // @ts-expect-error just ignore
+    replaceOptions.__RELOAD_SW__ = 'true'
+}
+
+if (selfDestroying)
+    pwaOptions.selfDestroying = selfDestroying
+
+
 export default defineConfig({
-  // prevent vite from obscuring rust errors
-  clearScreen: false,
-  // Tauri expects a fixed port, fail if that port is not available
-  server: {
-    strictPort: true,
-  },
-  plugins: [react(), VitePWA(manifestForPlugin)],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    // prevent vite from obscuring rust errors
+    clearScreen: false,
+    // Tauri expects a fixed port, fail if that port is not available
+    server: {
+        strictPort: true,
     },
-  },
-  // to access the Tauri environment variables set by the CLI with information about the current target
-  envPrefix: ["VITE_", "TAURI_PLATFORM", "TAURI_ARCH", "TAURI_FAMILY", "TAURI_PLATFORM_VERSION", "TAURI_PLATFORM_TYPE", "TAURI_DEBUG"],
-  build: {
-    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
-  },
+    plugins: [
+        react(),
+        VitePWA(manifestForPlugin),
+        replace(replaceOptions),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    // to access the Tauri environment variables set by the CLI with information about the current target
+    envPrefix: ["VITE_", "TAURI_PLATFORM", "TAURI_ARCH", "TAURI_FAMILY", "TAURI_PLATFORM_VERSION", "TAURI_PLATFORM_TYPE", "TAURI_DEBUG"],
+    build: {
+        // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+        target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+        // don't minify for debug builds
+        minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+        // produce sourcemaps for debug builds
+        sourcemap: !!process.env.TAURI_DEBUG,
+    },
 });
