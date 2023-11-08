00b8a26dae8ccb61a31ffb1897df500aabf5e189
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-11-08 11:08:56 +0630

    feat: add reload pwa

--- apps/myanmar_calendar/src/App.tsx
+++ apps/myanmar_calendar/src/App.tsx
@@ -4,9 +4,13 @@ import {Provider} from "react-redux";
 import {store} from "@/store";
 import AppSetupProvider from "@/components/providers/AppSetupProvider";
 import {Analytics} from "@vercel/analytics/react";
+import ReloadPrompt from "@/components/ReloadPrompt";
+import {Toaster} from "sonner";
 
-// document.documentElement.classList.add('dark')
+
 const Home = () => {
+    const date = '__DATE__';
+    console.log(date)
     return (
         <>
             <Analytics/>
@@ -15,6 +19,8 @@ const Home = () => {
                     <MyanmarCalendar2/>
                 </AppSetupProvider>
             </Provider>
+            <Toaster/>
+            <ReloadPrompt/>
         </>
     );
 };
