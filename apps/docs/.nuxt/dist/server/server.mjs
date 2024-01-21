31248c04d9264c3acba728083fbb41c6800a107b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-01-21 15:44:49 +0630

    refactor(docs): remove dead files

--- apps/docs/.nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///D:/Web%20Dev/Open%20source%20projects/myanmar-calendar/node_modules/.pnpm/@nuxt+vite-builder@3.8.1_@types+node@20.10.0_eslint@8.54.0_rollup@3.29.4_typescript@5.2.2_vue@3.3.8/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
